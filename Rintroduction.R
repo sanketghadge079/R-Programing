20b235386bd0f13714e183fee53e7a56deb25e8e
sanketghadge079 <sanketghadge079@gmail.com>
2024-07-28 23:48:18 +0530

    Add files via upload

---
+++ Rintroduction.R
@@ -0,0 +1,27 @@
+"Hello World"
+5
+#variable
+
+name1 = "Amit"
+name1
+
+name2 = "Neha"
+name2
+name = "Snehal"
+name
+
+age = 25
+age
+
+x = 21
+x
+
+num = "20"
+
+age = age+5
+
+num = num+5
+
+4*8
+4+(2*5)
+(4+2)/3
